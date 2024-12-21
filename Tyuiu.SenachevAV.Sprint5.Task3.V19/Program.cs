f99b9129f5c21d016aba3931c8d7e951aa9e64f1
Андрей Сеначев <andrey30-04-2005god@yandex.ru>
2024-12-21 14:43:45 +0500

    Task3

--- Tyuiu.SenachevAV.Sprint5.Task3.V19/Program.cs
+++ Tyuiu.SenachevAV.Sprint5.Task3.V19/Program.cs
@@ -4,6 +4,37 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        DataService ds = new();
+
+        Console.Title = "Спринт #5 | Выполнил: Сеначев А.В. | АСОиУБ-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл      *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #19                                                             *");
+        Console.WriteLine("* Выполнил: Сеначев Андрей Вячеславович | АСОиУБ 24-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в  *");
+        Console.WriteLine("* бинарный файл OutPutFileTask3.bin и вывести на консоль.                 *");
+        Console.WriteLine("* Округлить до трёх знаков после запятой.                                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x = 3;
+        Console.WriteLine("X = " + x);
+
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        string res = ds.SaveToFileTextData(x);
+
+        Console.WriteLine("Файл" + res);
+        Console.WriteLine("Создан!");
+
+        Console.ReadKey();
     }
 }
