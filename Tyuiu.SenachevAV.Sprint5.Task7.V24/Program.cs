c8c9eb55e6ecf105d8514042f3f1f4f03394c7b9
Андрей Сеначев <andrey30-04-2005god@yandex.ru>
2024-12-23 23:05:06 +0500

    Task7

--- Tyuiu.SenachevAV.Sprint5.Task7.V24/Program.cs
+++ Tyuiu.SenachevAV.Sprint5.Task7.V24/Program.cs
@@ -4,6 +4,31 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнил: Сеначев А.В. | АСОиУБ-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #24                                                             *");
+        Console.WriteLine("* Выполнил: Сеначев Андрей Вячеславович | АСОиУБ 24-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = @"C:\DataSprint5\InPutDataFileTask7V24.txt";
+        string res = File.ReadAllText(path);
+        Console.WriteLine(res);
+        Console.WriteLine("Файл находится тут: " + path);
+
+        Console.WriteLine("****************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                   *");
+        Console.WriteLine("****************************************************************");
+
+        string outputFilePath = ds.LoadDataAndSave(path);
+        Console.WriteLine($"Файл сохранен: {outputFilePath}");
+        Console.ReadKey();
     }
 }
