5e5e0f70003caaf6b1e54521163b81b608767b66
Андрей Сеначев <andrey30-04-2005god@yandex.ru>
2024-12-23 21:45:17 +0500

    Task6

---
+++ Tyuiu.SenachevAV.Sprint5.Task6.V20.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.SenachevAV.Sprint5.Task6.V20.Lib;
+
+namespace Tyuiu.SenachevAV.Sprint5.Task6.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask6V20.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.SenachevAV.Sprint5.Task6.V20/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.SenachevAV.Sprint5.Task6.V20.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #5 | Выполнил: Сеначев А.В. | АСОиУБ-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #22                                                             *");
+        Console.WriteLine("* Выполнил: Сеначев Андрей Вячеславович | АСОиУБ 24-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        DataService ds = new DataService();
+        string path = @"C:\DataSprint5\InPutDataFileTask6V20.txt";
+
+        Console.WriteLine("данные находятся в файле " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine(res);
+    }
+}
