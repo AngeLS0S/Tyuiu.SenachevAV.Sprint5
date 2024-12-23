a21fa3d299c4d5462d14bf16ba54354bef63c80b
Андрей Сеначев <andrey30-04-2005god@yandex.ru>
2024-12-23 23:02:33 +0500

    Task7

--- Tyuiu.SenachevAV.Sprint5.Task7.V24.Test/DataServiceTest.cs
+++ Tyuiu.SenachevAV.Sprint5.Task7.V24.Test/DataServiceTest.cs
@@ -8,6 +8,11 @@ namespace Tyuiu.SenachevAV.Sprint5.Task7.V24.Test
         [TestMethod]
         public void TestMethod1()
         {
+            string path = @"C:\DataSprint5\InPutDataFileTask7V24.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
         }
     }
 }
